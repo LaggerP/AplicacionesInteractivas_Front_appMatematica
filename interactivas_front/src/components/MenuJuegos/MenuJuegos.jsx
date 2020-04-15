2d31f67507e7135addc75064067350906e21e093
LaggerP <pablo_lagger@hotmail.com>
2020-04-15 15:26:49 -0300

    added new styles

--- interactivas_front/src/components/MenuJuegos/MenuJuegos.jsx
+++ interactivas_front/src/components/MenuJuegos/MenuJuegos.jsx
@@ -1,23 +1,29 @@
 import React, { Component } from 'react'
+import './MenuJuegos.scss';
 import CardJuego from './CustomComponent/CardJuego/CardJuego'
 import Button from '@material-ui/core/Button'
-import MenuNavbar from './MenuJuegosNavbar/MenuJuegosNavbar';
+import MenuJuegosNavbar from './MenuJuegosNavbar/MenuJuegosNavbar';
 import fractions from '../../assets/Images/fractions.png';
 import money from '../../assets/Images/monedas.png';
 import sums from '../../assets/Images/sumas.png';
-import './MenuJuegos.scss';
+import auth from "../../ProtectedRoutes/auth";
 
 export default class MenuJuegos extends Component {
+
+   getUser () {
+      return auth.getUser()
+   }
+
    render() {
       return (
          <div>
-            <MenuNavbar></MenuNavbar>
+            <MenuJuegosNavbar></MenuJuegosNavbar>
             <div className="bodyContainer">
-               <h1 className='Bienvenido1'>¡Hola {localStorage.getItem('sessionName')}!</h1>
+               <h1 className='Bienvenido1'>¡Hola {this.getUser()}!</h1>
                <h3 className='Bienvenido2'>Elegi cualquiera de nuestros juegos y comenzá a divertirte</h3>
                <ul className="bodyContainer-cards">
                   <Button>
-                     <li className="bodyContainer-cards-item">
+                     <li className="bodyContainer-cards-itemJuego">
                         <CardJuego
                         title="Monedas"
                         detail="Vamos al super juntos! En este juego vamos aprender
@@ -28,7 +34,7 @@ export default class MenuJuegos extends Component {
                      </li>
                   </Button>
                   <Button>
-                     <li className="bodyContainer-cards-item">
+                     <li className="bodyContainer-cards-itemJuego">
                         <CardJuego
                         title="Fracciones"
                         detail="En este juego aprenderemos a usar fracciones de una
@@ -39,7 +45,7 @@ export default class MenuJuegos extends Component {
                      </li>
                   </Button>
                   <Button>
-                     <li className="bodyContainer-cards-item">
+                     <li className="bodyContainer-cards-itemJuego">
                         <CardJuego
                         title="Sumas"
                         detail="El juego de sumas se trata en agilizar la mente.
